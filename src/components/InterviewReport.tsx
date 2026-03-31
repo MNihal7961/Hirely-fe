d958acb539ac98f14e34d55afae47b64f561c657
MNihal7961 <mnihal7961@gmail.com>
2026-03-31 22:26:46 +0530

    feat: create InterviewReport.tsx

---
+++ src/components/InterviewReport.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface InterviewReportProps {
+    report:any;
+}
+
+const InterviewReport: React.FC<InterviewReportProps> = ({ report }) => {
+  return (
+    <div>InterviewReport</div>
+  )
+}
+
+export default InterviewReport
