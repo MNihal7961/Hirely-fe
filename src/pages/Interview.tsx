ac0cf2c3cf3f033cde2af8fc0a5f168a47e21134
MNihal7961 <mnihal7961@gmail.com>
2026-03-31 22:26:49 +0530

    update Interview.tsx

--- src/pages/Interview.tsx
+++ src/pages/Interview.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import InterviewSetup from "../components/InterviewSetup";
+import InterviewScreen from "../components/InterviewScreen";
+import InterviewReport from "../components/InterviewReport";
 
 const Interview: React.FC = () => {
-  return (
-    <div>Interview</div>
-  )
-}
+  const [step, setStep] = useState<"setup" | "interview" | "report">("setup");
+  const [interviewData, setInterviewData] = useState<any>(null);
+  const [reportData, setReportData] = useState<any>(null);
 
-export default Interview
+  const handleSetup = (interviewData:any) => {
+    setInterviewData(interviewData);
+    setStep("interview");
+  };
+
+  const handleCompleteInterview = (report:any) => {
+    setReportData(report);
+    setStep("report");
+  };
+
+  const getContentBasedOnStep = () => {
+    switch (step) {
+      case "setup":
+        return <InterviewSetup onStartInterview={handleSetup} />;
+      case "interview":
+        return (
+          <InterviewScreen
+            onFinishInterview={handleCompleteInterview}
+            interviewData={interviewData}
+          />
+        );
+      case "report":
+        return <InterviewReport report={reportData} />;
+      default:
+        return null;
+    }
+  };
+  return (
+    <div className="min-h-screen bg-gray-50">{getContentBasedOnStep()}</div>
+  );
+};
+
+export default Interview;
